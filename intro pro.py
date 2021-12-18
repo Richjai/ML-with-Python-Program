2fb9bb0d597e3b5b0fb9831dadcf3a3a2be236de
Richjai <94793382+Richjai@users.noreply.github.com>
2021-12-18 04:24:04 -0800

    Add files via upload
    INTRODUCTION OF BASIC AIRTHMETIC OPERATION

---
+++ intro pro.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 07:38:14 2021
+
+@author: DELL
+"""
+2 + 2
+
+2 - 2
+
+2 * 2
+
+2 / 2
+
+x = 6
+
+y = 8
+
+z = x + y
+
+f = x - y
+
+g = x * y
+
+h= x / y
